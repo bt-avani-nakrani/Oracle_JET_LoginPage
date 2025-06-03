7294ca47da13458d04ceb2ee4ddc6af57d4ea746
bt-avani-nakrani <bt.avani.nakrani@gmail.com>
2025-06-03 13:22:45 +0530

    Delete src directory

--- src/src/js/appState.js
+++
@@ -1,7 +0,0 @@
-// js/appState.js
-define(['knockout'], function (ko) {
-    return {
-        appName: ko.observable("Please Login"),
-        userLogin: ko.observable("Guest")
-    };
-});
--- src/src/js/viewModels/notfound.js
+++
@@ -1,6 +0,0 @@
-define([], function () {
-    function NotFoundViewModel() {
-        // Empty or logging logic here
-    }
-    return new NotFoundViewModel();
-});
